53467befa972cb8690921f5de6ad4f868b339164
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-01 07:33:22 +0100

    Refactored.

--- solve.hpp
+++ solve.hpp
@@ -3,4 +3,11 @@
 #include<optional>
 using namespace std;
 
-optional<int> solve(vector<int> input, int pick = 2, int magic_number=2020);
+optional<int> solve(vector<int>::iterator, 
+		    vector<int>::iterator, 
+		    int pick = 2, int magic_number=2020);
+
+inline optional<int> solve(vector<int> input, int pick = 2, int magic_number=2020)
+{
+  return solve(input.begin(), input.end(), pick, magic_number);
+}
