8402bfb6ec352ca5e5136dc802faf0236cd01344
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-01 07:00:28 +0100

    2020 as magic_number

--- solve.hpp
+++ solve.hpp
@@ -1,12 +1,14 @@
 #pragma once
 #include<vector>
 using namespace std;
-int solve(vector<int> input)
+int solve(vector<int> input, int magic_number=2020)
 {
   auto first = input.begin();
   while(first != input.end())
     {
-      auto other = std::find_if(first, input.end(), [first](auto x){return x+(*first) == 2020;});
+      auto other = std::find_if(first,
+				input.end(),
+				[first, magic_number](auto x){return x+(*first) == magic_number;});
       if(other==input.end())
 	first++;
       else
