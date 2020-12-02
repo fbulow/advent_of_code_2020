533077e22b95254b9306feb2d3a41c07a8d77498
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-02 06:49:11 +0100

    Solve example data.

--- 2/main.cpp
+++ 2/main.cpp
@@ -23,3 +23,37 @@ TEST(letter_count, example)
 {
   EXPECT_EQ(5, letter_count('a', "aaxaaa"));
 }
+
+bool check(RuleAndPassword const &x)
+{
+  auto c = letter_count(x.rule.letter, x.password);
+  return (c>=x.rule.min) and (c<=x.rule.max);
+}
+
+TEST(check, pass)
+{
+  {
+    auto sut = example_input()[0];
+    EXPECT_TRUE(check(sut));
+  }
+  {
+    auto sut = example_input()[2];
+    EXPECT_TRUE(check(sut));
+  }
+}
+
+TEST(check, fail)
+{
+  auto sut = example_input()[1];
+  EXPECT_FALSE(check(sut));
+}
+
+int solve(vector<RuleAndPassword> const & data)
+{
+  return count_if(data.begin(), data.end(), [](auto x){return check(x);});
+}
+
+TEST(solve, example_data)
+{
+  EXPECT_EQ(2, solve(example_input()));
+}
