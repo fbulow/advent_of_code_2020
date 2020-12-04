404497f02cfa7987aa8a1f6faf644c56f06c6b21
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-04 06:10:36 +0100

    4; first test passing.

---
+++ 4/main.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include <functional>
+#include <string_view>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+TEST(hello, world)
+{
+  ASSERT_TRUE(true);
+}
+
