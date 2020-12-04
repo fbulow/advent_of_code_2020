84b7a2e8da3c289be29edb53aaa75ce4b0e91f9e
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-04 11:38:49 +0100

    5; Prepped.

---
+++ 5/main.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <regex>
+#include <string_view>
+#include <vector>
+
+using namespace std;
+
+TEST(hello, world)
+{
+  ASSERT_TRUE(false);
+}
