baef8e6abfb12c9e7c73d1afe0cffb37c6ff1f55
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-05 08:55:44 +0100

    Prepared day six.

---
+++ 6/main.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <fstream>
+
+//#include <algorithm>
+//#include <functional>
+//#include <iostream>
+//#include <regex>
+//#include <string_view>
+#include <vector>
+
+using namespace std;
+
+TEST(hello, world)
+{
+  ASSERT_TRUE(false);
+}
+
