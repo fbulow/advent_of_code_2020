21c2a071e09e39dab3568127dc7446c80d55d8bf
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-06 06:19:23 +0100

    6a; Reading example input.

--- 6/main.cpp
+++ 6/main.cpp
@@ -8,11 +8,60 @@
 //#include <regex>
 //#include <string_view>
 #include <vector>
+#include<set>
+
 
 using namespace std;
 
 TEST(hello, world)
 {
-  ASSERT_TRUE(false);
+  ASSERT_TRUE(true);
+}
+
+
+using Ans = set<char>;
+
+vector<Ans> getInput(string const &filename)
+{
+  vector<Ans> ret;
+  ifstream in(filename);
+  assert(in.is_open());
+  
+  Ans last;
+  string line;
+  while(getline(in, line))
+    {
+      if(line.empty())
+	{
+	  assert(not last.empty());
+	  ret.push_back(last);
+	  last.clear();
+	}
+      else
+	for(char c:line)
+	  last.insert(c);
+    }
+  if(not line.empty())
+    {
+      for(char c:line)
+	last.insert(c);
+      ret.push_back(last);
+    }
+  return ret;
+}
+
+TEST(getInput, example)
+{
+  auto const sut = getInput(EXAMPLE);
+  EXPECT_EQ(5, sut.size());
+  EXPECT_EQ(3, sut[0].size());
+  EXPECT_EQ(3, sut[1].size());
+  EXPECT_EQ(3, sut[2].size());
+  EXPECT_EQ(1, sut[3].size());
+  EXPECT_EQ(1, sut[4].size());
+
+  EXPECT_EQ(1, sut[4].size());
+  
+  EXPECT_TRUE(sut[4].contains('b'));
 }
 
