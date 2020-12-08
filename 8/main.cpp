ebabe7401b64a98e3225cd8423a89897f12af198
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-08 06:33:54 +0100

    8; Ready to start

---
+++ 8/main.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+//#include <regex>
+//#include <iostream>
+//#include <sstream>
+//#include <numeric>
+
+TEST(hello, world)
+{
+  EXPECT_TRUE(false);
+}
+using namespace std;
