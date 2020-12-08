156a8d0e3983ccc9755ce0ac8264c18cf76d2fc5
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-08 08:18:40 +0100

    8b; solved.

--- 8/main.cpp
+++ 8/main.cpp
@@ -25,7 +25,7 @@ istream& operator>>(istream &cin, Command &c)
   return cin;
 }
 
-ostream& operator<<(ostream &cout, Command &c)
+ostream& operator<<(ostream &cout, Command const &c)
 {
   cout	<< c.cmd
 	<< " "
@@ -35,6 +35,13 @@ ostream& operator<<(ostream &cout, Command &c)
 
 
 using Code = vector<Command>;
+
+ostream& operator<<(ostream& cout, Code const & c)
+{
+  for(auto const &x:c)
+    cout<<x<<endl;
+  return cout;
+}
 
 Code getCommands(string const &filename)
 {
@@ -70,6 +77,11 @@ struct GameBoy
     else if (code[pos].cmd == "nop")
       pos++;
   }
+  bool success() const
+  {
+    return pos>=code.size();
+  }
+  unsigned int run();
 };
 
 TEST(GameBoy, read_data)
@@ -93,18 +105,9 @@ TEST(GameBoy, step)
 
 pair<unsigned int, GameBoy> infiniteLoopSteps(GameBoy b)
 {
-  set<size_t> positions{};
-  while(b.pos<b.code.size())
-    {
-      if( positions.contains(b.pos) )
-	return {positions.size(), move(b)};
-      else
-	{
-	  positions.insert(b.pos);
-	  b.step();
-	}
-    }
-  assert(false);
+  auto steps = b.run();
+  assert(not b.success());//i.e. infinite loop
+  return {steps, move(b)};
 }
 
 TEST(GameBoy, infiniteLoopSteps)
@@ -117,6 +120,86 @@ TEST(GameBoy, infiniteLoopSteps)
 TEST(solution, a)
 {
   auto const sut = infiniteLoopSteps(GameBoy(getCommands(INPUT)));
-  EXPECT_EQ(5, get<GameBoy>(sut).accumulator);
+  EXPECT_EQ(1487, get<GameBoy>(sut).accumulator);
 }
 
+unsigned int GameBoy::run()
+{
+  set<size_t> positions{};
+  while(pos<code.size())
+    {
+      if( positions.contains(pos) )
+	return positions.size();
+      else
+	{
+	  positions.insert(pos);
+	  step();
+	}
+    }
+  return positions.size();
+}
+
+Command toggle(Command c)
+{
+  string &cmd = c.cmd;
+  if(cmd=="jmp")
+    cmd="nop";
+  else if (cmd=="nop")
+    cmd="jmp";
+  return c;
+}
+
+Code toggle(Code c, size_t index)
+{
+  c[index] = toggle(c[index]);
+  return c;
+}
+
+
+TEST(toggle, Command)
+{
+  EXPECT_EQ("jmp", toggle(Command{"nop",0}).cmd);
+  EXPECT_EQ("nop", toggle(Command{"jmp",0}).cmd);
+  EXPECT_EQ("acc", toggle(Command{"acc",0}).cmd);
+}
+
+optional<int> evaluate(Code const &c)
+{
+  GameBoy g(c);
+  g.run();
+  if(g.success())
+    return g.accumulator;
+  else
+    return {};
+}
+
+
+TEST(toggle, example)
+{
+  auto sut = getCommands(EXAMPLE);
+  sut[7] = toggle(sut[7]);
+  
+  EXPECT_EQ(8, evaluate(sut).value());
+
+  EXPECT_EQ(8, evaluate(toggle(getCommands(EXAMPLE), 7)).value());
+}
+
+int solveB(Code const &c, size_t index=0)
+{
+  assert(index<c.size());
+  auto x = evaluate(toggle(c, index));
+  if(not x)
+    return solveB(c, 1+index);
+  else
+    return x.value();
+}
+
+TEST(solve_b, example)
+{
+  EXPECT_EQ(8, solveB(getCommands(EXAMPLE)));
+}
+
+TEST(solve_b, input)
+{
+  EXPECT_EQ(1607, solveB(getCommands(INPUT)));
+}
