8c9cf2bb7fc31ba7926a9451e2371d30ed566502
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-09 07:51:18 +0100

    7b; solved.

--- 7/main.cpp
+++ 7/main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <sstream>
 #include <numeric>
+#include <cassert>
+#include <fstream>
 TEST(hello, world)
 {
   EXPECT_TRUE(true);
@@ -62,18 +64,18 @@ string join(auto a)
   return join(a.cbegin(), a.cend());
 }
 
-string getContainer(Words const &w)
+Words getContainer(Words const &w)
 {
   auto iter = find_if(w.cbegin(),
 		      w.cend(),
 		      [](auto const &x){return "bags"==x;});
   assert(*next(iter) == "contain");
-  return join(w.cbegin(), iter);
+  return {w.cbegin(), iter};
 }
 
 
 
-string getContainer(string const &str)
+Words getContainer(string const &str)
 {
   return getContainer(getWords(str));
 }
@@ -81,7 +83,7 @@ string getContainer(string const &str)
 TEST(getContainer, first)
 {
   EXPECT_EQ("light red",
-	    getContainer("light red bags contain 1 bright white bag, 2 muted yellow bags."));
+	    join(getContainer("light red bags contain 1 bright white bag, 2 muted yellow bags.")));
 }
 
 Words getContents(Words const &w)
@@ -166,3 +168,187 @@ TEST(splitContets, words)
   }
 }
 
+
+using Color = array<string,2>;
+struct Bags
+{
+  unsigned int count;
+  Color color;
+};
+
+struct Rule
+{
+  Color container;
+  vector<Bags> contents;
+  Rule(Words const &w)
+    :container([&](){ //evaluated immediately!
+      auto a = getContainer(w);
+      assert(2 == a.size());
+      return Color{a[0], a[1]};}())
+    ,contents([&](){ //evaluated immediately!
+      vector<Bags> ret;
+      for(auto x:splitContents(getContents(w)))
+	{
+	  if(not (x[0]=="no" and x[1]=="other"))
+	    ret.emplace_back(
+			     Bags{
+			       (unsigned int) stoi(x[0]),
+			       Color{x[1], x[2]}});
+	}
+      return ret;}())
+  {}
+  Rule(string const &s)
+    :Rule(getWords(s))
+  {}
+  bool containsColor(Color const &c) const
+  {
+    return find_if(contents.begin(),
+		   contents.end(),
+		   [&c](auto x){
+		     return x.color==c;}) != contents.end();
+  }
+  bool containsColorSet(set<Color> const &sc) const
+  {
+    return sc.end() != find_if(sc.begin(),
+			       sc.end(),
+			       [this](auto&c){return containsColor(c);});
+  }
+};
+
+TEST(Rule, ctor)
+{
+  Rule sut("muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.");
+  {
+    Color ref{"muted","yellow"};
+    EXPECT_EQ(ref, sut.container);
+  }
+  {
+    Color ref{"shiny","gold"};
+    EXPECT_EQ(ref, sut.contents[0].color);
+    EXPECT_EQ(2, sut.contents[0].count);
+  }
+  {
+    Color ref{"faded", "blue"};
+    EXPECT_EQ(ref, sut.contents[1].color);
+    EXPECT_EQ(9, sut.contents[1].count);
+  }
+}
+
+TEST(Rule, ctor_empty)
+{
+  Rule sut("faded blue bags contain no other bags.");
+  EXPECT_TRUE(sut.contents.empty());
+  
+}
+
+TEST(Rule, containsColor)
+{
+  Rule sut("muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.");
+  EXPECT_TRUE(sut.containsColor({"shiny", "gold"}));
+  EXPECT_FALSE(sut.containsColor({"faded", "yellow"}));
+  EXPECT_TRUE(sut.containsColor({"faded", "blue"}));
+}
+
+TEST(Rule, containsColorSet)
+{
+  Rule sut("muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.");
+  EXPECT_TRUE(sut.containsColorSet({Color{"shiny", "gold"}}));
+  EXPECT_FALSE(sut.containsColorSet({Color{"", ""}}));
+  EXPECT_TRUE(sut.containsColorSet({Color{"", ""}, Color{"shiny", "gold"}}));
+}
+
+set<Color> canContain(vector<Rule> const & rules, set<Color> &cs)
+{
+  auto before = cs.size();
+  for (auto const &r: rules)
+    if(r.containsColorSet(cs))
+      cs.insert(r.container);
+  if(before == cs.size())
+    return cs;
+  else
+    return canContain(rules, cs);
+}
+
+
+TEST(canContain, a)
+{
+  vector<Rule> data{
+    Rule("light red bags contain 1 bright white bag, 2 muted yellow bags."),
+    Rule("dark orange bags contain 3 bright white bags, 4 muted yellow bags."),
+    Rule("bright white bags contain 1 shiny gold bag."),
+    Rule("muted yellow bags contain 2 shiny gold bags, 9 faded blue bags."),
+    Rule("shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."),
+    Rule("dark olive bags contain 3 faded blue bags, 4 dotted black bags."),
+    Rule("vibrant plum bags contain 5 faded blue bags, 6 dotted black bags."),
+    Rule("faded blue bags contain no other bags."),
+    Rule("dotted black bags contain no other bags.")};
+
+
+  set<Color> cs{Color{"shiny", "gold"}};
+  EXPECT_EQ(4,
+	    canContain(data, cs).size()-1); //-1 for the initial bag.
+}
+
+vector<string> getInput(string const &filename)
+{
+  vector<string> ret;
+  ifstream in(filename);
+  assert(in.is_open());
+  string line;
+  while(getline(in, line))
+      ret.push_back(line);
+  
+  return ret;
+}
+
+
+TEST(canContain, file_input)
+{
+  vector<Rule> data;
+  for(string &s: getInput(EXAMPLE))
+    data.emplace_back(s);
+
+  set<Color> cs{Color{"shiny", "gold"}};
+  EXPECT_EQ(4,
+	    canContain(data, cs).size()-1); //-1 for the initial bag.
+}
+
+TEST(solution, a)
+{
+  vector<Rule> data;
+  for(string &s: getInput(INPUT))
+    data.emplace_back(s);
+
+  set<Color> cs{Color{"shiny", "gold"}};
+  EXPECT_EQ(172,
+	    canContain(data, cs).size()-1); //-1 for the initial bag.
+}
+
+
+unsigned int countBags(Color const &c, vector<Rule> const &r)
+{
+  unsigned int ret{1};//1 = this bag
+  for(Bags const& b: find_if(r.begin(),
+			     r.end(),
+			     [&c](auto x){return x.container == c;})->contents)
+    {
+      ret+= b.count * countBags(b.color, r);
+    }
+  return ret;
+}
+
+TEST(example, b)
+{
+  vector<Rule> data;
+  for(string &s: getInput(EXAMPLE))
+    data.emplace_back(s);
+  EXPECT_EQ(32, countBags({"shiny", "gold"}, data)-1);
+}
+
+TEST(solution, b)
+{
+  vector<Rule> data;
+  for(string &s: getInput(INPUT))
+    data.emplace_back(s);
+  EXPECT_EQ(39645, countBags({"shiny", "gold"}, data)-1);
+}
