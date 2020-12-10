79ce01010aebd206e50b5f3a4c179e4e86caeeda
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-10 05:57:28 +0100

    10a; initial.

---
+++ 10/main.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+//#include <regex>
+//#include <sstream>
+//#include <numeric>
+
+TEST(hello, world)
+{
+  EXPECT_TRUE(true);
+}
+using namespace std;
+
