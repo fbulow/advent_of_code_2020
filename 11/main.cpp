c2b3198188af3c7690eb1197f2b8b21d4e780c82
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-11 06:37:04 +0100

    11a; ready to start.

---
+++ 11/main.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <numeric>
+
+using namespace std;
+TEST(hello, world)
+{
+  EXPECT_FALSE(false);
+}
+
+
