418402036489de7748e48af49d7ced6db0aa277c
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-15 00:18:19 +0100

    14b; a solution.

--- 14/main.cpp
+++ 14/main.cpp
@@ -8,11 +8,55 @@
 // #include <cassert>
 using namespace std;
   
-using Value = unsigned long int;
+struct Value{
+  unsigned long int data;
+public:
+  Value(){}
+  Value(auto const &other)
+  :data(other)
+  {}
+
+  long unsigned int& operator()()
+  {return data;}
+  void operator*=(auto const &other)
+  {data*=other;}
+  void operator+=(Value const &other)
+  {data+=other.data;}
+  void operator+=(Value other)
+  {
+    data+= other.data;
+  }
+  void operator+=(int other)
+  {
+    data+= other;
+  }
+  Value operator+(Value other) const
+  {
+    other.data +=data;
+    return other;
+  }
+  bool operator<(Value const & other) const
+  {
+    return data<other.data;
+  }
+  
+  bool operator==(auto const &other) const
+  {return data==other.data;};
+  bool operator==(int other) const
+  {return data==other;};
+};
+
+
+
+ostream& operator<<(ostream& cout, Value const &v)
+{
+  cout<<v.data;
+  return cout;
+}
 
 struct Mask:string
 {
-  Value valueIfOneIs(char c)
+  Value valueIfOneIs(char c) const
   {
     Value ret=0;
     int mult=1;
@@ -29,7 +73,7 @@ struct Mask:string
     Value const positive = valueIfOneIs('1');
     Value const zero = valueIfOneIs('0');
 
-    return (v|positive) & (~zero);
+    return (v.data|positive.data) & (~zero.data);
   }
 };
 
@@ -47,9 +91,21 @@ Value getValue(string line)
   return stoll(line.substr(line.find('=')+1));
 }
 
+using Memory = map<Value, Value>;
+
+Value sumMemmory(Memory const &memory)
+{
+  return accumulate(memory.begin(),
+		    memory.end(),
+		    Value{0}, 
+		    [](Value const v, auto const &pair){
+		      return v+pair.second;
+		    });
+}
+
 Value solveA(ifstream && in)
 {
-  map<Value, Value> memory;
+  Memory memory;
   string line;
   Mask m;
   Value index;
@@ -63,16 +119,90 @@ Value solveA(ifstream && in)
 	    memory[getIndex(line)] = m.apply(getValue(line));
 	}
     }
-  return accumulate(memory.begin(),
-		    memory.end(),
-		    Value{0}, 
-		    [](Value const v, auto const &pair){
-		      return v+pair.second;
-		    });
+  return sumMemmory(memory);
 }
-
 
 Value solveA(string filename)
 {
   return solveA(ifstream(filename));
 }
+
+
+struct MemoryMask: Mask
+{};
+
+unsigned long int reverse(unsigned long int value, unsigned long int ret=0)
+{
+  if(value==0)
+    return ret;
+  else
+    return reverse(value/2, 2*ret+(value%2));
+}
+
+
+void setMemory(Memory &m,
+	       unsigned long int floating,
+	       unsigned long int a,
+	       unsigned long int address,
+	       Value const &v)
+{
+  if (floating == 0 and a == 0)
+    m[reverse(address)] = v;
+  else if(floating%2 == 0)
+    {
+      return setMemory(m,
+		       floating/2,
+		       a/2,
+		       2*address+a%2,
+		       v);
+    }
+  else
+    for(auto i : {0,1})
+      setMemory(m,
+		floating/2,
+		a/2,
+		2*address+i,
+		v);
+}
+
+void setMemory(Memory &m,
+	       MemoryMask const &mask,
+	       Value a,
+	       Value const &v)
+{
+  a.data|=mask.valueIfOneIs('1').data;
+  auto floating = mask.valueIfOneIs('X');
+  if(floating==0)
+    m[a]=v;
+  else
+    setMemory(m,
+	      floating.data,
+	      a.data,
+	      0,
+	      v);
+}
+
+Value solveB(ifstream &&in)
+{
+  Memory memory;
+  string line;
+  MemoryMask m;
+  Value index;
+  while(getline(in, line))
+    {
+      if(not line.empty())
+	{
+	  if(line.starts_with("mask = "))
+	    m = {line.substr(line.find('=')+2)};
+	  else if(line.starts_with("mem["))
+	    setMemory(memory,
+		      m,
+		      getIndex(line),
+		      getValue(line));
+	}
+    }
+  return sumMemmory(memory);
+}
+
+Value solveB(string filename)
+{return solveB(ifstream(filename));}
--- 14/tests.cpp
+++ 14/tests.cpp
@@ -55,3 +55,44 @@ TEST(sovle, a)
   ASSERT_EQ(165, solveA(EXAMPLE));
   cout<<"Solution a: "<<solveA(INPUT)<<endl;
 }
+
+TEST(sovle, B)
+{
+  ASSERT_EQ(208, solveB(EXAMPLE2));
+  //  cout<<"Solution b: "<<solveB(INPUT)<<endl;
+}
+
+
+TEST(setMemory, cellZero)
+{
+  Memory m;
+  setMemory(m,
+	    {"000000000000000000000000000000000000"},
+	    {0},
+	    10);
+  EXPECT_EQ(10, m[0]);
+}
+
+TEST(setMemory, overwrite)
+{
+  {
+    Memory m;
+    setMemory(m,
+	      {"000000000000000000000000000000000001"},
+	      {0},
+	      10);
+    EXPECT_EQ(10, m[1]);
+  }
+}
+
+TEST(setMemory, floating)
+{
+  {
+    Memory m;
+    setMemory(m,
+	      {"00000000000000000000000000000000000X"},
+	      {0},
+	      10);
+    EXPECT_EQ(2, m.size());
+  }
+}
