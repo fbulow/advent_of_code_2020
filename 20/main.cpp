3e1e8e7ec321a128106f70ff7cd08f1b7d7df8cc
Fredrik Bülow <fredrik.bulow@gmail.com>
2020-12-22 21:07:44 +0100

    20a; solved.

--- 20/main.cpp
+++ 20/main.cpp
@@ -6,17 +6,28 @@
 #include <sstream>
 #include <numeric>
 #include<list>
+#include<ranges>
 using namespace std;
 
-using I = long long int;
+using I = unsigned int;
 
-class Tile{
+template<class T>
+struct Sides{
+  T top;    //left to right 
+  T bottom; //left to right 
+  T left;   //top to bottom
+  T right;  //top to bottom
+
+  vector<T> sides() const
+  {
+    return {top, bottom, left, right};
+  }
+};
+
+class Tile : public Sides<string>
+{
 public:
   I nr;
-  string top;    //left to right 
-  string bottom; //left to right 
-  string left;   //top to bottom
-  string right;  //top to bottom
 
   Tile(vector<string> const & data)
   {
@@ -58,8 +69,153 @@ public:
 
     left = slask;
     reverse(begin(left), end(left));
-    
+  }
+};
+
+struct Pile
+{
+  vector<Tile> data;
+  Pile(string filename)
+  {
+    vector<string> data;
+    ifstream in(filename);
+    assert(in.is_open());
+    string line;
+    while(getline(in, line))
+      data.push_back(line);
+
+    auto a = data.begin();
+    auto b = find(a, data.end(), "");
+    while(b!=data.end())
+      {
+	this->data.emplace_back(Tile(vector<string>{a,b}));
+	a=next(b);
+	b=find(a,data.end(), "");
+      }
+  }
+  size_t size() const
+  {return data.size();}
+
+  optional<Tile> getTile(I nr)
+  {
+    auto x= find_if(data.begin(),
+		    data.end(),
+		    [nr](auto &t){return t.nr==nr;});
+    if(x==data.end())
+      return {};
+    else
+      return {*x};
   }
 
   
+  Tile const& operator[](size_t i) const
+  {
+    return data[i];
+  }
+
+  auto begin() const
+  {
+    return data.begin();
+  }
+  auto end() const
+  {
+    return data.end();
+  }
+
+  auto erase(auto iterator)
+  {
+    return data.erase(iterator);
+  }
+  
 };
+
+struct Coord{
+  int column;
+  int row;
+  bool operator==(auto const &other) const
+  {return (row==other.row) and (column == other.column);}
+  
+  bool operator<(auto const &other) const
+  {
+    if(column<other.column) return true;
+    else if(column == other.column) return row < other.row;
+    else return false;
+  }
+};
+
+Coord up   (Coord c){ c.row-=1;	   return c;}
+Coord down (Coord c){ c.row+=1;	   return c;}
+Coord left (Coord c){ c.column-=1; return c;}
+Coord right(Coord c){ c.column+=1; return c;}
+
+void insertNear(set<Coord> &near, Coord const &c)
+{
+  near.emplace(up   (c));
+  near.emplace(down (c));
+  near.emplace(left (c));
+  near.emplace(right(c));
+}
+
+int encodeBinary(string const &s)
+{
+  int sum=0;
+  for(auto x:s)
+    sum=2*sum+(('#'==x)?1:0);
+  return sum;
+}
+  
+
+using Key = unsigned int;
+
+Key bigKey(string s)
+{
+  string r(s.rbegin(), s.rend());
+  return max(encodeBinary(s), encodeBinary(r));
+}
+
+bool isCorner(Tile const &t, set<unsigned int> const& edges)
+{
+  auto s = t.sides();
+  
+  return 2==count_if(s.begin(),
+		     s.end(),
+		     [&edges](auto x){
+		       return edges.contains(bigKey(x));
+		     });;
+}
+
+set<Key> getEdges(Pile const &p)
+{
+  map<Key, int>    edgeCount;// edge, count;  
+  for(Tile t: p)
+    for(auto ts:t.sides())
+	edgeCount[bigKey(ts)]++;
+  
+  auto x =edgeCount
+    |views::filter([](auto &x){return x.second==1;})
+    |views::transform([](auto &x){return x.first;});
+
+  return set<Key>(x.begin(), x.end());
+}
+
+
+
+auto solveA(string filename)
+{
+
+  Pile p(filename);
+  auto edges = getEdges(p);
+
+  auto corners = p.data
+    |views::filter([edges](auto tile){return isCorner(tile, edges);});
+
+  
+  return accumulate(corners.begin(),
+		    corners.end(),
+		    (unsigned long long int) 1,
+		    [](unsigned long long int product, auto tile)
+		    {
+		      product*=tile.nr;
+		      return product;
+		    });
+}    
--- 20/tests.cpp
+++ 20/tests.cpp
@@ -97,7 +97,63 @@ TEST(tile, flip)
   EXPECT_EQ(ref.bottom, sut.bottom	);
   EXPECT_EQ(ref.left  , sut.left	);
   EXPECT_EQ(ref.right , sut.right	);
-
-    
 }
 
+TEST(Pile, read)
+{
+  Pile sut(EXAMPLE);
+  EXPECT_EQ(9, sut.size());
+}
+
+// TEST(Puzzle, construct)
+// {
+//   Puzzle sut(Pile(EXAMPLE));
+
+//   EXPECT_EQ(8, sut.p.size());
+// }
+
+TEST(maxSum, first)
+{
+  EXPECT_EQ(0b1001000000, encodeBinary("#..#......"));
+  EXPECT_EQ(0b1001000000, bigKey("#..#......"));
+  EXPECT_EQ(0b1001000000, bigKey("......#..#"));
+}
+
+TEST(isCorner, topLeft)
+{
+  auto ans = isCorner(Tile({"Tile 1951:",
+		      "#.##...##.",
+		      "#.####...#",
+		      ".....#..##",
+		      "#...######",
+		      ".##.#....#",
+		      ".###.#####",
+		      "###.##.##.",
+		      ".###....#.",
+		      "..#.#..#.#",
+		      "#...##.#.."}),
+		 {
+		   bigKey("#.##...##."),
+		   bigKey("##.#..#..#")});
+
+  ASSERT_TRUE(ans);
+}
+
+TEST(getEdges, example)
+{
+  Pile example(EXAMPLE);
+  auto sut = getEdges(example);
+  EXPECT_EQ(12, sut.size());
+
+  EXPECT_TRUE(isCorner(example.getTile(1951).value(), sut));
+  EXPECT_FALSE(isCorner(example.getTile(2729).value(), sut));
+}
+
+
+TEST(solve, a)
+{
+  EXPECT_EQ(20899048083289, solveA(EXAMPLE));
+  auto ans = solveA(INPUT);
+  EXPECT_EQ(30425930368573, ans);
+  cout<<"Solution a: "<<ans<<endl;
+}
