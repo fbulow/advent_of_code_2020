d392fdf90c399c0f250d4c46c15c3dce58e632b8
Fredrik Bülow <fredrik.bulow@gmail.com>
2021-01-02 22:36:11 +0100

    11b; solved after restart.

--- 11 Seating System/main.cpp
+++ 11 Seating System/main.cpp
@@ -16,59 +16,70 @@ using namespace std;
 
 using I = unsigned long int;
 
+struct Dir
+{
+  int row{0};
+  int column{0};
+};
+
 struct Coord
 {
   size_t row{0};
   size_t column{0};
+
+  Coord& operator+=(Dir const &d)
+  {
+    row+=d.row;
+    column+=d.column;
+    return *this;
+  }
 };
 
-using Dir=Coord;
-
-class Room
+template<class T>
+class Grid
 {
-  int Nrows{0};
-  int Ncolumns{0};
-  vector<char> data;
+protected:
+  size_t Nrows{0};
+  size_t Ncolumns{0};
+  vector<T> data;
 public:
-  Room(string filename)
+  Grid(size_t Nrows,
+       size_t Ncolumns)
+    :Nrows{Nrows}
+    ,Ncolumns{Ncolumns}
   {
-    ifstream in(filename);
-    assert(in.is_open());
-    string line;
-    deque<string> tmp;
-    assert(getline(in, line));
-    assert(line.size()!=0);
-    Ncolumns = line.size();
-    tmp.push_back(line);
-    
-    while(getline(in, line))
-      {
-        if(not line.empty())
-          {
-            Nrows++;
-            tmp.push_back(line);
-          }
-      }
-    Nrows++;
     data.resize(Nrows*Ncolumns);
-    auto it = data.begin();
-    for(string &row: tmp)
-      it = copy(row.begin(), row.end(), it);
-  }
-
-  unsigned int count(char c) const
-  {
-    return std::count(data.cbegin(),
-                      data.cend(),
-                      c);
   }
 
   
-  char operator[](size_t i)
+  Grid(deque<string> const & lines)
+    :Grid(lines.size(),
+          lines.front().size())
   {
-    return data[i];
+    data.resize(Nrows*Ncolumns);
+    auto it = data.begin();
+    for(string const &row: lines)
+      it = copy(row.begin(), row.end(), it);
   }
-  char& operator[](Coord const &c)
+
+  size_t countRows() const
+  {
+    return Nrows;
+  }
+    
+  size_t countColumns() const
+  {
+    return Ncolumns;
+  }
+  
+  T& operator[](Coord const &c)
+  {
+    assert(c.row< Nrows);
+    assert(c.column< Ncolumns);
+
+    return data[c.row*Ncolumns + c.column];
+  }
+  T const & operator[](Coord const &c) const
   {
     assert(c.row< Nrows);
     assert(c.column< Ncolumns);
@@ -76,26 +87,165 @@ public:
     return data[c.row*Ncolumns + c.column];
   }
 
-  char visible(Coord c, Dir d) const 
+  auto begin()
   {
-    return '#';
+    return data.begin();
+  }
+  auto end()
+  {
+    return data.end();
+  }
+  
+};
+
+
+class Room : public Grid<char>
+{
+public:
+  friend bool     operator==(Room const &a, Room const &b);
+  friend ostream& operator<<(ostream& out, Grid<char> const &g);
+  Room(string filename)
+    :Room([filename]()
+    {
+      ifstream in(filename);
+      assert(in.is_open());
+      return in;
+    }())
+  {}
+  
+  Room(istream &&in)
+    :Room([this](istream &&in) //evaluated immediately
+    {
+      string line;
+      deque<string> lines;
+      assert(getline(in, line));
+      assert(line.size()!=0);
+      Ncolumns = line.size();
+      lines.push_back(line);
+    
+      while(getline(in, line))
+        {
+          if(not line.empty())
+            {
+              lines.push_back(line);
+            }
+        }
+      return lines;
+    }(move(in)))
+  {}
+
+  Room(deque<string> const &lines)
+    :Grid<char>(lines)
+  {}
+  
+  unsigned int count(char c) const
+  {
+    return std::count(data.cbegin(),
+                      data.cend(),
+                      c);
+  }
+  
+  bool inRoom(Coord const &c) const
+  {
+    return (c.row<Nrows) and (c.column<Ncolumns);
+  }
+  
+  char visible(Coord c, Dir const &d) const 
+  {
+    c+=d;
+    while(inRoom(c))
+      {
+        if( (*this)[c]!='.' )
+          return (*this)[c];
+        c+=d;
+      }
+    return '.';
   }
   
   unsigned int countVisible(Coord c, char state='#')
   {
-    array<Coord,8> const directions({{0,1}, {0, -1},
-                                     {1,0}, {-1, 0},
-                                     {1,1}, {-1,1}, {1,-1}, {-1,-1}});
+    array<Dir,8> const directions({{0,1}, {0, -1},
+                                   {1,0}, {-1, 0},
+                                   {1,1}, {-1,1}, {1,-1}, {-1,-1}});
 
     
     return count_if(directions.cbegin(),
                     directions.cend(),
-                    [this, c, state](Coord const &dir)
+                    [this, c, state](Dir const &dir)
                     {
                       return visible(c, dir)==state;
                     });
     
   }
-  
 };
 
+bool operator==(Room const &a, Room const &b)
+{
+  return a.data==b.data;
+}
+
+ostream& operator<<(ostream& out, Grid<char> const &g)
+{
+  out<<endl;
+  for(size_t row=0;row<g.countRows();row++)
+    {
+      for(size_t column=0;column<g.countColumns();column++)
+        out<<g[{row, column}];
+      out<<endl;
+    }
+  return out;
+}
+ostream& operator<<(ostream& out, Room const &r)
+{
+  return out<<static_cast<Grid<char>>(r);
+}
+
+struct Cell
+{
+  char current;
+  unsigned int countOccupied;
+
+  char nextState() const
+  {
+    if(current=='L' and countOccupied==0)
+      //If a seat is empty (L) and there are no occupied seats
+      //adjacent to it, the seat becomes occupied.
+      return '#';
+    else if(current=='#' and countOccupied>=5)
+      //If a seat is occupied (#) and four or more seats adjacent to
+      //it are also occupied, the seat becomes empty.
+      return 'L';
+    else
+      //Otherwise, the seat's state does not change.
+      return current;
+  }
+};
+
+Room step(Room r)
+{
+  Grid<Cell> heat(r.countRows(), r.countColumns());
+  for(size_t row=0; row< r.countRows(); row++)
+    for(size_t column=0; column< r.countColumns(); column++)
+      {
+        Coord c{row,column};
+        heat[c] = Cell(r[c], r.countVisible(c,'#'));
+      }
+
+  transform(heat.begin(),
+            heat.end(),
+            r.begin(),
+            [](Cell const &c){return c.nextState();});
+  return r;
+}
+
+size_t solutionB(string filename)
+{
+  Room a(filename);
+  auto b = step(a);
+  while(not (a==b))
+    {
+      a=b;
+      b=step(a);
+    }
+  return a.count('#');
+}
--- 11 Seating System/tests.cpp
+++ 11 Seating System/tests.cpp
@@ -13,25 +13,197 @@ TEST(Room, ctor)
 }
 
 
+TEST(Room, countVisible)
+{
+  {
+    Room sut({
+        ".......#.",
+        "...#.....",
+        ".#.......",
+        ".........",
+        "..#L....#",
+        "....#....",
+        ".........",
+        "#........",
+        "...#....."});
+
+    ASSERT_EQ('L', sut[Coord(4,3)]);
+    ASSERT_EQ(8, sut.countVisible(Coord(4,3)));
+  }
+  {
+    Room sut({
+        ".......#.",
+        ".........",
+        ".#.......",
+        ".........",
+        "..#L....#",
+        "....#....",
+        ".........",
+        "#........",
+        "...#....."});
+
+    ASSERT_EQ('L', sut[Coord(4,3)]);
+    ASSERT_EQ(7, sut.countVisible(Coord(4,3)));
+    ASSERT_EQ(1, sut.countVisible(Coord(4,3), '.'));
+  }
+}
+
 TEST(Room, visible)
 {
-  Room(
-.......#.
-...#.....
-.#.......
-.........
-..#L....#
-....#....
-.........
-#........
-...#.....
 
+    Room sut({
+        ".......A.",
+        "...B.....",
+        ".c.......",
+        ".........",
+        "..DL....E",
+        "....F....",
+        ".........",
+        "G........",
+        "...H....."});
+
+    ASSERT_EQ('A', sut.visible(Coord(4,3), Dir(-1,1)));
+    
+}
+     
+
+TEST(Room, eq)
+{
+
+    Room sut({
+        ".......A.",
+        "...B.....",
+        ".c.......",
+        ".........",
+        "..DL....E",
+        "....F....",
+        ".........",
+        "G........",
+        "...H....."});
+
+    ASSERT_EQ(sut, sut);
+    
+    Room removedL({
+        ".......A.",
+        "...B.....",
+        ".c.......",
+        ".........",
+        "..D.....E",
+        "....F....",
+        ".........",
+        "G........",
+        "...H....."});
+
+    ASSERT_FALSE(sut == removedL);
+}
+
+TEST(Cell, def)
+{
+  //If a seat is empty (L) and there are no occupied seats adjacent to
+  //it, the seat becomes occupied.
+
+  EXPECT_EQ('#', Cell('L', 0).nextState());
+
+  EXPECT_EQ('L', Cell('L', 1).nextState());
+  EXPECT_EQ('L', Cell('L', 2).nextState());
+  EXPECT_EQ('L', Cell('L', 3).nextState());
+  EXPECT_EQ('L', Cell('L', 4).nextState());
+  EXPECT_EQ('L', Cell('L', 5).nextState());
+  EXPECT_EQ('L', Cell('L', 6).nextState());
+  
+  //If a seat is occupied (#) and four or more seats adjacent to it
+  //are also occupied, the seat becomes empty.
+
+  //but...
+  
+  //Also, people seem to
+  //be more tolerant than you expected: it now takes five or more
+  //visible occupied seats for an occupied seat to become empty
+  //(rather than four or more from the previous rules).
+  
+  EXPECT_EQ('#', Cell('#', 1).nextState());
+  EXPECT_EQ('#', Cell('#', 2).nextState());
+  EXPECT_EQ('#', Cell('#', 3).nextState());
+  EXPECT_EQ('#', Cell('#', 4).nextState());
+  EXPECT_EQ('L', Cell('#', 5).nextState());
+  EXPECT_EQ('L', Cell('#', 6).nextState());
+
+  //Otherwise, the seat's state does not change.
+  EXPECT_EQ('.', Cell('.', 0).nextState());
+  EXPECT_EQ('.', Cell('.', 1).nextState());
+  EXPECT_EQ('.', Cell('.', 2).nextState());
+  EXPECT_EQ('.', Cell('.', 3).nextState());
+  EXPECT_EQ('.', Cell('.', 4).nextState());
+  EXPECT_EQ('.', Cell('.', 5).nextState());
+  EXPECT_EQ('.', Cell('.', 6).nextState());
 }
 
 
-// TEST(solution, b)
-// {
-//   ASSERT_EQ(26, solutionA(EXAMPLE));
-//   cout<<"Solution b: "<<solutionA(INPUT)<<endl;
-// }
+
+TEST(solution, b)
+{
+  ASSERT_EQ(26, solutionB(EXAMPLE));
+  cout<<"Solution b: "<<solutionB(INPUT)<<endl;
+}
+
+
+TEST(step, example)
+{
+  Room before({
+      "L.LL.LL.LL",
+      "LLLLLLL.LL",
+      "L.L.L..L..",
+      "LLLL.LL.LL",
+      "L.LL.LL.LL",
+      "L.LLLLL.LL",
+      "..L.L.....",
+      "LLLLLLLLLL",
+      "L.LLLLLL.L",
+      "L.LLLLL.LL"
+    });
+
+  Room after({
+      "#.##.##.##",
+      "#######.##",
+      "#.#.#..#..",
+      "####.##.##",
+      "#.##.##.##",
+      "#.#####.##",
+      "..#.#.....",
+      "##########",
+      "#.######.#",
+      "#.#####.##"
+    });
+  EXPECT_EQ(after, step(before));
+}
+
+TEST(step, failing)
+{
+  Room before({
+      "#.##.##.##",
+      "#######.##",
+      "#.#.#..#..",
+      "####.##.##",
+      "#.##.##.##",
+      "#.#####.##",
+      "..#.#.....",
+      "##########",
+      "#.######.#",
+      "#.#####.##"
+    });
+
+  Room after({
+      "#.LL.LL.L#",
+      "#LLLLLL.LL",
+      "L.L.L..L..",
+      "LLLL.LL.LL",
+      "L.LL.LL.LL",
+      "L.LLLLL.LL",
+      "..L.L.....",
+      "LLLLLLLLL#",
+      "#.LLLLLL.L",
+      "#.LLLLL.L#"});
+  EXPECT_EQ(after, step(before));
+}
+
 
