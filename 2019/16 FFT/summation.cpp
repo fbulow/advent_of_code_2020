f15d456081ec0cd0e19fb317077c488386cde007
Fredrik Bülow <fredrik.bulow@gmail.com>
2021-04-24 23:34:03 +0200

    refactored.

--- 2019/16 FFT/summation.cpp
+++ 2019/16 FFT/summation.cpp
@@ -11,16 +11,8 @@ Summation::Summation(vector<int> signal)
 unsigned int Summation::size(unsigned int stride)
 {
   assert(stride>0);
-  int total = signal.size();
-  unsigned int ret{1};
-  total-=(stride-1);
-  while(total>0)
-    {
-      ret++;
-      total-=stride;
-        
-    }
-  return ret;
+  auto rest = (signal.size()-(stride-1));
+  return 1+rest/stride + (rest%stride>0?1:0);
 }
   
 int Summation::operator()(unsigned int segment, unsigned int stride)
