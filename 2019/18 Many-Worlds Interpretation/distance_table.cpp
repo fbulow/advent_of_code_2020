2a3b0d9ffe3409b020cd1d6ca813276e6fbf3544
Fredrik Bülow <fredrik.bulow@gmail.com>
2021-05-03 13:31:32 +0200

    DistanceTable::get_nodes

--- 2019/18 Many-Worlds Interpretation/distance_table.cpp
+++ 2019/18 Many-Worlds Interpretation/distance_table.cpp
@@ -4,19 +4,71 @@
 
 using Steps = DistanceTable::Steps;
 
+DistanceTable::Nodes DistanceTable::get_nodes(char c) const
+{
+  Nodes ret;
+  for(Distance const &x:data)
+    {
+      if(x.from == c)
+        ret.push_back({x.to, x.steps});
+      if(x.to == c)
+        ret.push_back({x.from, x.steps});
+    }
+  return ret;
+}
+
+
+
+TEST(nodes, none)
+{
+  DistanceTable sut;
+  ASSERT_EQ(0, sut.get_nodes('a').size());
+}
+TEST(Nodes, get_two)
+{
+  DistanceTable sut;
+
+  sut.add_distance('a', 'b', 10);
+  sut.add_distance('a', 'c', 5);
+
+  EXPECT_EQ(2, sut.get_nodes('a').size());
+  EXPECT_EQ(1, sut.get_nodes('b').size());
+  EXPECT_EQ(1, sut.get_nodes('c').size());
+}
+
+void DistanceTable::remove(char c)
+{
+  
+  // static vector<Distance> = data;
+  // data.resize(0);
+  // auto beg=data.begin();
+  // while(beg!=data.end());
+  // auto ret = find_if(beg, data.end(),
+  //                    [from, to](Distance const &d)
+  //                    {
+  //                      return ( (d.from == min(from,to)) and 
+  //                               (d.to == max(from,to)) );
+  //                    });
+  
+}
+
+
 void DistanceTable::add_distance(char from, char to, Steps distance)
 {
-  data.push_back({from, to, distance});
+  data.push_front({from, to, distance});
 }
 
 Steps DistanceTable :: get_distance( char from, char to )
 {
-  return find_if(data.begin(), data.end(),
+  auto ret = find_if(data.begin(), data.end(),
                  [from, to](Distance const &d)
                  {
                    return ( (d.from == min(from,to)) and 
                             (d.to == max(from,to)) );
-                 })->steps;
+                 });
+  if(ret == data.end()) return 0;
+  else
+    return ret->steps;
 }
 
 TEST(ctor, no_arg)
@@ -35,7 +87,17 @@ TEST(DistanceTable, add_distance)
 
   sut.add_distance('a', 'c', 5);
   EXPECT_EQ(5, sut.get_distance('a', 'c'));
+}
 
+TEST(DistanceTable, remove_destination)
+{
+  DistanceTable sut;
+
+  sut.add_distance('a', 'b', 10);
+  sut.add_distance('a', 'c', 5);
+  sut.remove('a');
+
+  EXPECT_EQ(15, sut.get_distance('b', 'c'));
 }
 
 
--- 2019/18 Many-Worlds Interpretation/distance_table.hpp
+++ 2019/18 Many-Worlds Interpretation/distance_table.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
-
+#include <optional>
+#include <forward_list> 
 using namespace std;
 
 class DistanceTable{
@@ -21,8 +22,12 @@ private:
       
 };
     
-  vector<Distance> data;
+  forward_list<Distance> data;
 public:
   void add_distance(char from, char to, Steps distance);
   Steps get_distance( char from, char to );
+  void remove(char c);
+
+  using Nodes=vector<pair<char, Steps>>;
+  Nodes get_nodes(char) const;
 };
