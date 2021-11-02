4b64ab129707e2ab33f18039cd99f52b85d089bc
Fredrik Bülow <fredrik.bulow@gmail.com>
2021-11-02 22:31:04 +0100

    Position struct.

---
+++ 2018/20 A Regular Map/position.cpp
@@ -0,0 +1,24 @@
+#include "position.hpp"
+#include <cassert>
+Position Position::step(char direction) const
+{
+  switch(direction)
+  {
+  case 'N':
+    return {row-1, col};
+  case 'S':
+    return {row+1, col};
+  case 'E':
+    return {row,   col+1};
+  case 'W':
+    return {row,   col-1};
+  }
+  assert(false);  
+}
+
+
+void Position::operator=(Position const & other)
+{
+  row = other.row;
+  col = other.col;
+}
---
+++ 2018/20 A Regular Map/position.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include<compare>
+
+using namespace std;
+
+struct Position{
+  unsigned int row;
+  unsigned int col;
+
+  Position step(char direction) const;
+
+  auto operator<=>(Position const & other) const
+  {
+    auto r = row <=> other.row;
+    if (r==0)
+      return col <=> other.col;
+    else
+      return r;
+  }
+    
+  void operator=(Position const & other);
+};
+
