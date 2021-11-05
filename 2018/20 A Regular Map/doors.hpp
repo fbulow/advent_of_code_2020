930cb27d90bf1bcca7b72c7447571e8d3f7668d0
Fredrik Bülow <fredrik.bulow@gmail.com>
2021-11-05 22:06:16 +0100

    Refactored.

--- 2018/20 A Regular Map/doors.hpp
+++ 2018/20 A Regular Map/doors.hpp
@@ -7,36 +7,71 @@
 
 using namespace std;
 
-class Doors
-{
-  list<array<Position, 2>> data;
+class Door{
+  Position a;
+  Position b;
 public:
-  void push(Position a, Position b)
+  Door(Position const & a, Position const &b)
+    :a(min(a,b))
+    ,b(max(a,b))
   {
-    assert(not (a==b));
-    for(auto x:data)
-      {
-        if (x == array<Position, 2>({a,b}) or
-            x == array<Position, 2>({b,a}))
-          return;
-      }
-    data.push_back({a,b});
+    assert(a!=b);
   }
 
+  bool has(Position const &p) const
+  {
+    return (a==p) or (b==p);
+  }
+    
+  
+  auto operator<=>(Door const & other) const
+  {
+    auto r = a <=> other.a;
+    if (r==0)
+      return b <=> other.b;
+    else
+      return r;
+  }
+  bool operator==(Door const &other) const = default;
+  Position other(Position const &p) const 
+  {
+    if(a==p)
+      return b;
+    else
+      {
+        assert (b==p);
+        return a;
+      }
+  }
+};
+
+class Doors
+{
+  list<Door> data;
+public:
+  void push(Door d)
+  {
+    for(auto x:data)
+      {
+        if(d==x)
+          return;
+      }
+    data.push_back(d);
+  }
+  void push(Position a, Position b)
+  {
+    push(Door(a,b));
+  }
+  
   vector<Position> pop(Position c)
   {
     vector<Position> ret;
     ret.reserve(4);
     for(auto it = data.begin(); it!=data.end();)
       {
-        if((*it)[0]==c)
+        if(it->has(c))
           {
-            ret.push_back((*it)[1]);
-            it = data.erase(it);
-          }
-        else if((*it)[1]==c)
-          {
-            ret.push_back((*it)[0]);
+            ret.push_back(it->other(c));
             it = data.erase(it);
           }
         else
