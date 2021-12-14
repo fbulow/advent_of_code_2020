43f467c1380abd625cc585e53948532d95f84f4c
Fredrik Bülow <fredrik.bulow@gmail.com>
2021-12-14 07:26:48 +0100

    14 b

--- 2021/14/main.py
+++ 2021/14/main.py
@@ -1,3 +1,4 @@
+import pdb
 data = open('input').readlines()
 template = data[0].strip()
 
@@ -25,10 +26,50 @@ def substNtimes(template, N):
     cnt = dict()
     for x in ans:
         cnt[x] = cnt.get(x,0) + 1
+    return cnt
+
+def answer(cnt):
     return max(cnt.values()) - min(cnt.values())
 
-assert(substNtimes(template, 10) == 3306)
+
+
+assert(answer(substNtimes(template, 10)) == 3306)
     
-print("a: ", substNtimes(template, 10))
+print("a: ", answer(substNtimes(template, 10)))
+
+def keyCount(template):
+    x= template
+    ret=dict()
+    for i in range(len(template)-1):
+        key = x[i:i+2]
+        ret[key]= ret.get(key,0)+1
+    return ret
+
+def elementCount(keyCount):
+    twice = {}
+    for key in keyCount:
+        for letter in key:
+            twice[letter] = twice.get(letter,0)+keyCount[key]
+    for key in twice:
+        twice[key]= twice[key]//2 +twice[key]%2
+    return twice
 
 
+def step(kc):
+    ret=dict()
+    for key in kc:
+        a = key[0]+rules[key]
+        b = rules[key]+key[1]
+        if a in rules:
+            ret[a] = ret.get(a,0)+kc[key]
+        if b in rules:
+            ret[b] = ret.get(b,0)+kc[key]
+    return ret
+        
+def stepTemplate(template,N):
+    kc = keyCount(template)
+    for i in range(N):
+        kc = step(kc)
+    return kc
+
+print(answer(elementCount(stepTemplate(template, 40))))
