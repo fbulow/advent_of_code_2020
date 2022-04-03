0e9bf34a7c31aa6a261ca089c3876b5325ea9873
Fredrik Bülow <fredrik.bulow@gmail.com>
2022-04-03 08:41:00 +0200

    FIX(20) Solve b

--- 2021/20_trenchmap/main.cpp
+++ 2021/20_trenchmap/main.cpp
@@ -3,11 +3,11 @@
 #include "impl.hh"
 
 
-uint twoInversionSteps(Key const &k,
+Board twoInversionSteps(Key const &k,
                        Board const& b)
 {
   return iteration(k.invertedInput(),
-                   iteration(k.inverted(), b)).size();
+                   iteration(k.inverted(), b));
 }
 
   
@@ -19,7 +19,7 @@ uint solutionA(Key k, Board b)
   // last is .
   assert(*k.begin());
   assert(not *prev(k.end()));
-  return twoInversionSteps(k, b);
+  return twoInversionSteps(k, b).size();
 }
 
 
@@ -36,3 +36,30 @@ TEST(solution, a)
   EXPECT_EQ(5419, ans);
   cout<<"Solution A: "<<ans<<endl;
 }
+
+uint solutionB(Key k, Board b)
+{
+  // This solution assumes that the "void space" toggles between . and
+  // # in every move. This happens if the fist element is # and the
+  // last is .
+  auto N=25;
+  while((N--)>0)
+    {
+      b = twoInversionSteps(k, b);
+    }
+  return b.size();
+}
+
+
+TEST(solution, b)
+{
+  
+  Key k;
+  Board b;
+  ifstream in(INPUT);
+  in >>k>>b;
+  auto ans = solutionB(k,b);
+  EXPECT_EQ(17325, ans);
+  cout<<"Solution B: "<<ans<<endl;
+}
+
