a75674caf71fa2719a2a6f862332c1918d8766d0
Fredrik Bülow <fredrik.bulow@gmail.com>
2022-08-09 23:29:51 +0200

    -

--- 2021/23_amphipod/solver.cc
+++ 2021/23_amphipod/solver.cc
@@ -4,58 +4,116 @@
 #include<gmock/gmock.h>
 #include<optional>
 
-class Board{
-public:
-  enum class Status {Done, Failed};
-  virtual Status status() const = 0;
-  virtual int score() const {return 0;}
+class Move{
+
 };
 
-std::optional<int> Solver(Board const &b)
+class Board{
+public:
+  bool isDone() const{return true;}
+  int score() const{return 0;}
+  std::vector<Move> moves() const {return {};}
+  Board apply(Move const & m) const {return {};}
+};
+
+template<class BOARD>
+std::optional<int> Solver(BOARD const &b)
 {
-  switch(b.status())
+  auto m = b.moves();
+  if(m.empty())
     {
-    case Board::Status::Done:
-      return {b.score()};
-    case Board::Status::Failed:
-      return {};
+      if (b.isDone())
+	return {b.score()};
+      else
+	return {};
     }
-  assert(false);
+  else
+    {
+      return Solver(b.apply({}));
+    }
 }
 
-TEST(Solver, return_current_score_if_done)
+using namespace testing;
+
+TEST(Solver, recursive_call_on_return_value_of_apply)
 {
   struct :Board{
-    Status status() const override
-    {
-      return Status::Done;
+    std::vector<Move> moves() const {return {Move{}};}
+    Board apply(Move const & m) const{
+      ::Board ret;
+      assert(ret.isDone());
+      assert(ret.score() == 0 );
+
+      return ret;
     }
-    int score() const {return 5;}
+    int score() const{
+      return 5; // not zero
+    }
+
   } b;
-  
-  ASSERT_THAT(Solver(b).value(), testing::Eq(5));
+  ASSERT_THAT(Solver(b).value(), Eq(0));
+}
+
+TEST(Solver, do_not_call_apply_if_moves_empty)
+{
+  static int count{0};
+  struct :Board{
+    bool isDone() const {return true;}
+    std::vector<Move> moves() const {return {};}
+    Board apply(Move const & m) const{
+      count++;
+      return ::Board::apply(m);
+    }
+  }b;
+
+  Solver(b);
+  ASSERT_THAT(count, Eq(0));
+}
+
+
+TEST(Solver, call_apply_if_moves_not_empty)
+{
+  static int count{0};
+  struct :Board{
+    bool isDone() const {return true;}
+    std::vector<Move> moves() const {return {Move{}};}
+    Board apply(Move const & m) const{
+      count++;
+      return ::Board::apply(m);
+    }
+  }b;
+
+  Solver(b);
+  ASSERT_THAT(count, Eq(1));
+}
+
+TEST(Solver, return_score_if_no_moves_and_isdone)
+{
+  struct :Board{
+    bool isDone() const {return true;}
+    std::vector<Move> moves() const {return{};}
+    int score() const{return 5;}
+  }b;
+
+  ASSERT_THAT(Solver(b).value(), Eq(5));
 }
 
 TEST(Solver, return_nothing_if_failed)
 {
   struct :Board{
-    Status status() const override
-    {
-      return Status::Failed;
-    }
-  } b;
+    bool isDone() const {return false;}
+  }b;
   ASSERT_FALSE(Solver(b));
 }
 
-
-TEST(Solver, return_score_if_done)
+TEST(Solver, return_current_score_if_done)
 {
   struct :Board{
-    Status status() const override
-    {
-      return Status::Done;
-    }
-  } b;
-  ASSERT_TRUE(Solver(b));
+    bool isDone() const {return true;}
+    int score() const { return 5; }
+  }b;
+  ASSERT_THAT(Solver(b).value(), testing::Eq(5));
 }
+
+
 
