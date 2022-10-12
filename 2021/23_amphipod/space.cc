a27fed5dc60767c3bb63d7d00c46b6e3fa91dc4f
Fredrik Bülow <fredrik.bulow@gmail.com>
2022-10-12 11:39:47 +0200

    Removing test - empty if only residents was such a hack

--- 2021/23_amphipod/space.cc
+++ 2021/23_amphipod/space.cc
@@ -3,13 +3,6 @@
 #include <gmock/gmock.h>
 
 using namespace testing;
-TEST(Space, empty_if_only_residents)
-{
-  Space sut(2, 'A');
-  ASSERT_THAT(sut.getTop(), Eq('.'));
-  sut.put('A');
-  ASSERT_THAT(sut.getTop(), Eq('.'));
-}
 
 TEST(Space, depth_reduced_when_adding_resident_to_empty)
 {
