9df1e2f5ba6206cc9af18fc7e4a5b37d8e394842
Fredrik Bülow <fredrik.bulow@gmail.com>
2022-10-13 14:43:08 +0200

    Handle fail, disabled example (too slow)

--- 2021/23_amphipod/solver.cc
+++ 2021/23_amphipod/solver.cc
@@ -18,3 +18,47 @@ TEST(solution_b, one_step)
   EXPECT_THAT(
 	      Solver(b), Eq(3000));
 }
+
+TEST(solution_b, all_steps)
+{
+  Board b(
+"#############"
+"#..........D#"
+"###A#B#C#.###"
+"  #A#B#C#D#  "
+"  #A#B#C#D#  "
+"  #A#B#C#D#  "
+"  #########  ");
+  EXPECT_THAT(
+	      Solver(b), Eq(3000));
+}
+
+TEST(Solver, handles_a_fail)
+{
+  Board b(
+"#############"
+"#.A.A.......#"
+"###B#.#.#.###"
+"  #D#.#.#.#  "
+"  #D#.#.#.#  "
+"  #A#.#.#.#  "
+"  #########  "
+	  );
+  EXPECT_FALSE(Solver(b));
+
+}
+
+TEST(DISABLED_solution_b, example)
+{
+  Board b(
+"#############"
+"#...........#"
+"###B#C#B#D###"
+"  #D#C#B#A#  "
+"  #D#B#A#C#  "
+"  #A#D#C#A#  "
+"  #########  "
+	  );
+  EXPECT_THAT(
+	      Solver(b), Eq(44169));
+}
