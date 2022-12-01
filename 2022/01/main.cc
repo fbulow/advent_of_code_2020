636b813d339504ac14bcb74880c6750562d99df2
Fredrik Bülow <fredrik.bulow@gmail.com>
2022-12-01 08:34:54 +0100

    solution A #2022-1

---
+++ 2022/01/main.cc
@@ -0,0 +1,68 @@
+#include <istream>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+int getElfCalories(istream& input)
+{
+  if(input.eof())
+    return 0;
+  string s;
+  getline(input, s);
+  if(s.empty())
+    return 0;
+  int val;
+  istringstream(s)>>val;
+  if(input.fail())
+    return 0;
+  else
+    return val+getElfCalories(input);
+}
+
+int solA(istream& input, int ret=0)
+{
+  auto next = getElfCalories(input);
+  if(input.eof())
+    return ret;
+  else
+    return solA(input, max(ret, next));
+}
+
+#include<gtest/gtest.h>
+
+TEST(getElfCalories, single)
+{
+  istringstream in("1000");
+  EXPECT_EQ(1000, 
+	    getElfCalories(in));
+}
+
+TEST(getElfCalories, two)
+{
+  istringstream in("1000\n2000");
+  EXPECT_EQ(3000, 
+	    getElfCalories(in));
+}
+
+TEST(getElfCalories, trailing_newline)
+{
+  istringstream in("1000\n");
+  EXPECT_EQ(1000, 
+	    getElfCalories(in));
+}
+
+TEST(getElfCalories, two_groups)
+{
+  istringstream in("1000\n\n2000");
+  EXPECT_EQ(1000, 
+	    getElfCalories(in));
+  EXPECT_EQ(2000, 
+	    getElfCalories(in));
+}
+
+TEST(solution, A)
+{
+  ifstream input(INPUT);
+  EXPECT_EQ(71924, solA(input));
+}
