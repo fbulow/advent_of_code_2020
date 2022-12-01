54be2498e34c687bb48d0e17f81dc94b179c4ce7
Fredrik Bülow <fredrik.bulow@gmail.com>
2022-12-01 08:55:16 +0100

    solution B #2022-1

--- 2022/01/main.cc
+++ 2022/01/main.cc
@@ -1,7 +1,9 @@
 #include <istream>
 #include <sstream>
 #include <fstream>
-
+#include <vector>
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
 int getElfCalories(istream& input)
@@ -27,6 +29,22 @@ int solA(istream& input, int ret=0)
     return ret;
   else
     return solA(input, max(ret, next));
+}
+
+int solB(istream& input)
+{
+  vector<int> values;
+  while(not input.eof())
+    values.push_back(getElfCalories(input));
+  partial_sort(values.begin(),
+	       next(values.begin(),4),
+	       values.end(),
+	       [](int a, int b){
+		 return a>b;});
+  return accumulate(values.begin(),
+		    next(values.begin(), 3),
+		    0);
+		    
 }
 
 #include<gtest/gtest.h>
@@ -65,4 +83,32 @@ TEST(solution, A)
 {
   ifstream input(INPUT);
   EXPECT_EQ(71924, solA(input));
+}
+
+TEST(examlpe, B)
+{
+  istringstream input(
+		      R"(1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+)");		      
+  EXPECT_EQ(45000, solB(input));
+}
+
+
+TEST(solution, B)
+{
+  ifstream in(INPUT);
+  EXPECT_EQ(45000, solB(in));
 }
