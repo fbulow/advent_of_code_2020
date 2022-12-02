1b551e725626487c03e708dac395e8c008a486c4
Fredrik Bülow <fredrik.bulow@gmail.com>
2022-12-02 06:47:07 +0100

    02 draft

---
+++ 2022/02/dream.cc
@@ -0,0 +1,25 @@
+int solA()
+{
+  auto scores = getAllLines<Score>();
+  return accumulate(scores);
+    
+}
+
+int iWon(string s)
+{
+
+}
+
+int intrisicValue(s)
+{
+
+}
+
+struct Score{
+  int value;
+  Score(string s)
+    :value(iWon(s) + intrisicValue(s))
+  {
+  }
+};
+
