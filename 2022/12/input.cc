f7fe77d4023e820ef56d26452292731ccf161d57
Fredrik Bülow <fredrik.bulow@gmail.com>
2022-12-15 23:42:42 +0100

    move stuff to .cc #12

--- 2022/12/input.cc
+++ 2022/12/input.cc
@@ -11,3 +11,22 @@ bool Input::canMoveFromTo(Coord from, Coord to) const
 
   return heightIncrease <=1;
 }
+
+vector<Coord> Input::adjacent(Coord c)
+{
+  vector<Coord> ret;
+  for(Coord cc : c.adjacent())
+    if(onBoard(cc))
+      if(canMoveFromTo(c, cc))
+	ret.push_back(cc);
+  return ret;
+}
+
+bool Input::onBoard(Coord const & c)
+{
+  if(c.row() < data.size()) //Also false if row() is negative due to signed/unsigned arithmetics!
+    if (c.col() < data[c.row()].size())
+      return true;
+  return false;
+}
+  
--- 2022/12/input.hh
+++ 2022/12/input.hh
@@ -15,23 +15,7 @@ public:
     :data(move(data))
   {}
 
-  bool onBoard(Coord const & c)
-  {
-    if(c.row() < data.size()) //Also false if row() is negative due to signed/unsigned arithmetics!
-      if (c.col() < data[c.row()].size())
-	return true;
-    return false;
-  }
-  
+  bool onBoard(Coord const & c);
   bool canMoveFromTo(Coord from, Coord to) const;
-
-  vector<Coord> adjacent(Coord c) //STARTED
-  {
-    vector<Coord> ret;
-    for(Coord cc : c.adjacent())
-      if(onBoard(cc))
-	if(canMoveFromTo(c, cc))
-	  ret.push_back(cc);
-    return ret;
-  }
+  vector<Coord> adjacent(Coord c);
 };
