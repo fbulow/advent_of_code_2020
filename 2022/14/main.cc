84659a5b8c2cf75543a55847332f6741e7145b9c
Fredrik Bülow <fredrik.bulow@gmail.com>
2023-03-05 16:36:42 +0100

    Reading input: washline #14a

--- 2022/14/main.cc
+++ 2022/14/main.cc
@@ -4,3 +4,30 @@
 using namespace std;
 using namespace testing;
 
+void washLine(string &s)
+{
+  for(char &c:s)
+    switch(c)
+      {
+      case '-':
+      case '>':
+      case ',':
+	c=' ';
+      default:
+	{}
+      }
+}
+TEST(washLine, example)
+{
+  string s = "503,4 -> 502,4";
+  washLine(s);
+
+  istringstream in(s);
+  int a,b,c,d;
+  in>>a>>b>>c>>d;
+
+  EXPECT_THAT(a, Eq(503));
+  EXPECT_THAT(b, Eq(4));
+  EXPECT_THAT(c, Eq(502));
+  EXPECT_THAT(d, Eq(4));
+}
