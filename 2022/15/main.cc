04f48315619521e1ac623b033a7cc2b47e0303f0
Fredrik Bülow <fredrik.bulow@gmail.com>
2023-03-11 20:34:45 +0100

    Solved 15b

--- 2022/15/main.cc
+++ 2022/15/main.cc
@@ -13,7 +13,86 @@
 using namespace std;
 using namespace testing;
 
+optional<Int> findDarkSpot(OnOff onOff, Int a, Int b)
+{
+  onOff[a];
+  onOff[b];
+  auto end = onOff.find(b);
 
+  int sensors{0};
+  auto inIntervall = [a,b](auto x){return (x>=a) and (x<=b);};
+
+  for(auto const &x: onOff)
+    {
+      sensors+=x.second;
+      if(sensors==0 and inIntervall(x.first))
+	return {x.first};
+    }
+  return nullopt;
+}
+
+unsigned long long int tuningFrequency(unsigned long long int x, unsigned long long int y)
+{
+  return 4000000*x+y;
+}
+
+auto solutionB(vector<Sensor> const &sensors, Int minVal, Int maxVal)
+{
+  for(Int row = minVal; row<=maxVal; row++)
+    {
+      auto ret = findDarkSpot(getOnOff(sensors, row), minVal, maxVal);
+      if(ret)
+	return tuningFrequency(ret.value(), row);
+    }
+  assert(false);
+}
+
+auto solutionB(string filename, Int minVal, Int maxVal)
+{
+  return solutionB(getData(filename), minVal, maxVal);
+}
+
+TEST(solutionB, input)
+{
+  EXPECT_THAT(solutionB(INPUT, 0, 4000000),
+	      Eq(13673971349056));
+}
+
+TEST(tuningFrequency, something_large)
+{
+  EXPECT_THAT(tuningFrequency(3418492, 3349056),
+	    Eq(13673971349056));
+}
+
+
+TEST(solutionB, example)
+{
+  EXPECT_THAT(solutionB(EXAMPLE, 0, 20),
+	      Eq(56000011));
+}
+
+TEST(findDarkSpot, last_is_dark)
+{
+  OnOff s;
+  s.insert({-1, 1});
+  s.insert({20, -1});
+  EXPECT_THAT(findDarkSpot(s, 0,20).value(), Eq(20));
+}
+
+TEST(findDarkSpot, first_is_dark)
+{
+  OnOff s;
+  s.insert({1, 1});
+  EXPECT_THAT(findDarkSpot(s, 0,20).value(), Eq(0));
+}
+
+TEST(findDarkSpot, no_dark_spot)
+{
+  OnOff s;
+  s.insert({-1, 1});
+  s.insert({21, -1});
+  EXPECT_FALSE(findDarkSpot(s, 0,20));
+}
 
 TEST(crapOnRow, example)
 {
