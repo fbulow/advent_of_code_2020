e3177a0647e652d3fc89da973aa5ea6646597d85
Fredrik Bülow <fredrik.bulow@gmail.com>
2023-04-15 12:21:34 +0200

    Navigator, destination_removed_when_goto_is_reached and Navigator, ctor

--- 2022/16/main.cc
+++ 2022/16/main.cc
@@ -3,10 +3,56 @@
 #include <vector>
 #include "read_input.hh"
 #include "row.hh"
+#include <ranges>
+#include <algorithm>
 
 using namespace testing;
 using namespace std;
 
+using Valve = string;
+using Destinations = vector<Valve>;
+
+class Navigator{
+  Destinations destinations_;
+public:
+  Navigator(Destinations destinations)
+    :destinations_(move(destinations))
+  {}
+  Destinations const & getDestinations() const
+  {return destinations_;}
+  Navigator goTo(Valve v) const
+  {
+    return Navigator(
+		     [this, v] //Evaluated immediately!
+		     {
+		       assert( ! destinations_.empty() );
+		       Destinations ret;
+		       ret.reserve(destinations_.size()-1);
+		       ranges::copy_if(destinations_, back_inserter(ret), [v](auto x){return x!=v;});
+		       return ret;
+		     }());
+
+  }
+};
+
+TEST(Navigator, ctor)
+{
+  Navigator sut({"AA", "BB"});
+  EXPECT_THAT(sut.getDestinations(),
+	      ElementsAre("AA","BB"));
+
+  EXPECT_THAT(sut.goTo("AA").getDestinations(),
+	      ElementsAre("BB"));
+}
+
+TEST(Navigator, destination_removed_when_goto_is_reached)
+{
+  
+  EXPECT_THAT(
+	      Navigator({"AA", "BB"}).goTo("AA").getDestinations(),
+	      ElementsAre("BB")
+	      );
+}
 
 
 TEST(scrubbed, example)
