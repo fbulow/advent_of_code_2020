ecb52c20bdefe9e5017d0cf4a91178afb7f50e92
Fredrik Bülow <fredrik.bulow@gmail.com>
2023-10-21 22:16:05 +0200

    Region impl in cc

--- 2022/16/regions.cc
+++ 2022/16/regions.cc
@@ -1,1 +1,33 @@
 #include "regions.hh"
+
+
+Regions::Regions(Set all)
+  :outer_(std::move(all))
+{}
+
+Regions Regions::startAt(Valve const &v) const
+{
+  auto ret = *this;
+  ret.outer_.erase(v);
+  ret.edge_.insert(v);
+    
+  return ret;
+}
+Regions Regions::moveTo(Valve const &v) const
+{
+  Regions ret({});
+  for(auto const &x: inner_)
+    ret.inner_.insert(x);
+  for(auto const &x: edge())
+    ret.inner_.insert(x);
+  ret.edge_.insert(v);
+  for(auto const &x: outer())
+    if(x!=v)
+      ret.outer_.insert(x);
+    
+  return ret;
+}
+
+Regions::Set Regions::outer() const {return outer_;}
+Regions::Set Regions::edge()  const {return edge_;}
+Regions::Set Regions::inner() const {return inner_;}
--- 2022/16/regions.hh
+++ 2022/16/regions.hh
@@ -3,40 +3,18 @@
 #include<set>
 
 class Regions{
-public:
   using Set = std::set<Valve>;
   Set outer_;
   Set edge_;
   Set inner_;
+public:
 
-  Regions(Set all)
-    :outer_(std::move(all))
-  {}
+  Regions(Set all);
+  
+  Regions startAt(Valve const &v) const;
+  Regions moveTo(Valve const &v) const;
 
-  Regions startAt(Valve const &v) const
-  {
-    auto ret = *this;
-    ret.outer_.erase(v);
-    ret.edge_.insert(v);
-    
-    return ret;
-  }
-  Regions moveTo(Valve const &v) const
-  {
-    Regions ret({});
-    for(auto const &x: inner_)
-      ret.inner_.insert(x);
-    for(auto const &x: edge())
-      ret.inner_.insert(x);
-    ret.edge_.insert(v);
-    for(auto const &x: outer())
-      if(x!=v)
-	ret.outer_.insert(x);
-    
-    return ret;
-  }
-
-  Set outer() const {return outer_;}
-  Set edge()  const {return edge_;}
-  Set inner() const {return inner_;}
+  Set outer() const;
+  Set edge()  const;
+  Set inner() const;
 };
