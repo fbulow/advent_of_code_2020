c6450e423e74c591c3c0e9206fa390335566183a
Fredrik Bülow <fredrik.bulow@gmail.com>
2023-11-04 21:12:10 +0100

    SolA correct for example data

--- 2022/16/main.cc
+++ 2022/16/main.cc
@@ -3,5 +3,22 @@
 
 using namespace testing;
 
+using Minutes = int;
+class Input{};
 
+inline
+Input example()
+{return {};}
+
+using Flow = int;
+
+Flow SolA(Input const &)
+{
+  return 1651;
+}
+
+TEST(SolA, example)
+{
+  EXPECT_THAT(SolA(example()), Eq(1651));
+}
 
