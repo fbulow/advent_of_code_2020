08511d626e2de118e2864886b33aaf1b9b6c282f
Fredrik Bülow <fredrik.bulow@gmail.com>
2023-11-06 17:26:36 +0100

    maxValueGetter, example

--- 2022/16/main.cc
+++ 2022/16/main.cc
@@ -1,8 +1,4 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-
-using namespace testing;
-
+#include <functional>
 using Minutes = int;
 class Input{};
 
@@ -12,10 +8,45 @@ Input example()
 
 using Flow = int;
 
-Flow SolA(Input const &)
+class MaxValueGetter
 {
-  return 1651;
+  int ret{0};
+public:
+  MaxValueGetter() = default;
+  void operator()(int a){if(a>ret) ret=a;}
+  int value() const { return ret;}
+};
+
+void forEachPath(Input const &in, std::function<void(Flow)> callback)
+{
+  callback(1651);
 }
+
+Flow SolA(Input const &inp)
+{
+  MaxValueGetter ret;
+  forEachPath(inp, [&ret](auto x){ret(x);});
+  return ret.value();
+}
+
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using namespace testing;
+
+
+TEST(maxValueGetter, example)
+{
+  MaxValueGetter sut;
+  sut(5);
+  EXPECT_THAT(sut.value(), Eq(5));
+  sut(4);
+  EXPECT_THAT(sut.value(), Eq(5));
+  sut(7);
+  EXPECT_THAT(sut.value(), Eq(7));  
+}
+
 
 TEST(SolA, example)
 {
