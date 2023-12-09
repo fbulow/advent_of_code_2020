104455c429257980e88a4d7735885115fbeb04be
Fredrik Bülow <fredrik.bulow@gmail.com>
2023-12-09 06:51:57 +0100

    9b

---
+++ 2023/09/impl.cc
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <set>
+#include <map>
+#include <cassert>
+#include <vector>
+using namespace std;
+
+using Int = long long;
+
+auto nextValue(vector<Int> v)
+{
+  if( all_of(v.begin(),
+	     v.end(),
+	     [](auto x){return x==0;}) )
+    return Int(0);
+  else
+    {
+      auto last = *v.rbegin();
+      for(size_t i=1; i<v.size(); i++)
+	v[i-1] = v[i]-v[i-1];
+      v.resize(v.size()-1);
+      return last + nextValue(v);
+    }
+}
+
+auto solA(istream &&in)
+{
+  Int ret = 0;
+  string line;
+  while(getline(in,line))
+    if(!line.empty())
+      {
+	istringstream is(line);
+	Int i;
+	vector<Int> v;
+	while(!(is>>i).fail())
+	  v.push_back(i);
+	ret += nextValue(v);
+      }
+  return ret;
+}
+
+auto solB(istream &&in)
+{
+  Int ret = 0;
+  string line;
+  while(getline(in,line))
+    if(!line.empty())
+      {
+	istringstream is(line);
+	Int i;
+	vector<Int> v;
+	while(!(is>>i).fail())
+	  v.push_back(i);
+	reverse(v.begin(), v.end());
+	ret += nextValue(v);
+      }
+  return ret;
+}
---
+++ 2023/09/tests.cc
@@ -0,0 +1,37 @@
+
+#include "impl.cc"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+using namespace testing;
+
+TEST(nextValue, example)
+{
+    EXPECT_THAT(nextValue({0, 0}), Eq(0));
+    EXPECT_THAT(nextValue({1, 1}), Eq(1));
+    EXPECT_THAT(nextValue({0, 1, 2}), Eq(3));
+
+    EXPECT_THAT(nextValue({0, 3, 6, 9, 12, 15}), Eq(18));
+}
+
+TEST(solA, example)
+{
+  ASSERT_THAT(
+	      solA(ifstream(EXAMPLE)),
+	      Eq(114));
+  ASSERT_THAT(
+	      solA(ifstream(INPUT)),
+	      Eq(1772145754));
+}
+
+
+TEST(solB, example)
+{
+  ASSERT_THAT(
+	      solB(ifstream(EXAMPLE)),
+	      Eq(2));
+  ASSERT_THAT(
+	      solB(ifstream(INPUT)),
+	      Eq(0));
+}
+
