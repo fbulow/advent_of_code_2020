1194fb96a72455af19c013befa9121a881cdd8c5
Fredrik Bülow <fredrik.bulow@gmail.com>
2024-12-03 06:51:29 +0100

    2b

--- 2024/03/sol.py
+++ 2024/03/sol.py
@@ -14,15 +14,46 @@ def mul(s):
 def line_value(line): return sum(mul(x) for x in re.findall("mul\\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\\)", line))
     
 
+
+
+def solA():
+    return sum(line_value(line) for line in open("input").readlines())
+print(solA())
+
+
+def commands(line):
+    return re.findall("mul\\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\\)|do\\(\\)|don't\\(\\)", line)
+
+def eval_commands(commands):
+    ret = 0
+    active = True
+    for cmd in commands:
+        if cmd == 'do()':
+            active = True
+        elif cmd == "don't()":
+            active = False
+        elif active:
+            ret += mul(cmd)
+    return ret
+
+def all_commands(lines):
+    return sum((commands(line) for line in lines), start = [])
+def solB():
+    return eval_commands(all_commands(open("input").readlines()))
+
+
 class Test_(unittest.TestCase):
     def test_mul(self):
         self.assertEqual(8, mul('mul(2,4)'))
         self.assertEqual(88, mul('mul(11,8)'))
     def test_line_value(self):
         self.assertEqual(161, line_value(line))
+    def test_line_value_b(self):
+        self.assertEqual(48, line_value_b("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"))
+    def test_solA(self):
+        self.assertEqual(167090022, solA())
+    def test_solB(self):
+        self.assertNotEqual(93994894, solB())
+        
+print(solB())    
 
-
-def solA():
-    return sum(line_value(line) for line in open("input").readlines())
-print(solA())        
-unittest.main()
