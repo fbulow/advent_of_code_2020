24d7c8c3f0a97aa34a96d4f852edd4581b476cb2
Fredrik Bülow <fredrik.bulow@gmail.com>
2024-12-07 08:51:24 +0100

    7b

--- 2024/07/sol.py
+++ 2024/07/sol.py
@@ -8,33 +8,48 @@ def puzzle_input(filename="input"):
     with open(filename,'r') as f:
         return [x.strip() for x in f.readlines()]
 
-def rec(result, total, *tail):
+def add(a,b):
+    if not ((isinstance(a,int)) and (isinstance(b,int))):
+        pdb.set_trace()
+    return a+b
+
+def mul(a,b):
+    if not ((isinstance(a,int)) and (isinstance(b,int))):
+        pdb.set_trace()
+    return a*b
+    
+def joina(a,b):
+    return int(str(a)+str(b))
+    
+def rec(result, total, *tail,
+        operators = [add, mul]):
+                     
     if len(tail) == 0:
         return result == total
     else:
-        return \
-            rec(result,
-                total + tail[0],
-                *tail[1:]) or \
-            rec(result,
-                total * tail[0],
-                *tail[1:])
-            
-def is_repairable(line):
+        for val in (f(total, tail[0]) for f in operators):
+            if rec(result, val, *tail[1:], operators = operators):
+                return True
+        return False
+    
+def is_repairable(line, **kw):
     first, *rest = [int(x) for x in line.replace(':', ' ').split()]
-    if rec(*[int(x) for x in line.replace(':', ' ').split()]):
+    if rec(*[int(x) for x in line.replace(':', ' ').split()], **kw):
         return first
     else:
         return 0
-    
     
 def solA(lines):    
     return sum(is_repairable(line) for line in lines)
 
 def solB(lines):
-    pass
+    return sum(is_repairable(line, operators=[add, mul, joina]) for line in lines)
 
 class Test_(TestCase):
+    def test_solB(self):
+        self.assertEqual(
+            11387,
+            solB(puzzle_input('example')))
     def test_solA(self):
         self.assertEqual(
             3749,
