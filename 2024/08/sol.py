ca46c0b904c9f1010c975b0a02bddb267f94c106
Fredrik Bülow <fredrik.bulow@gmail.com>
2024-12-08 08:22:32 +0100

    8b

--- 2024/08/sol.py
+++ 2024/08/sol.py
@@ -19,9 +19,18 @@ def frequency_table(lines):
                 ret[char] = ret.get(char, [])+[(row, col)]
     return ret
 
-def antinodes(a,b):
+def resonant_nodes(a,b, rcount):
+    ret = set()
     dx, dy = tuple( p-q for p, q in zip(a,b))
-    return set(((a[0]+dx, a[1]+dy), (b[0]-dx, b[1]-dy)))
+    for _ in range(rcount):
+        a = (a[0]+dx, a[1]+dy)
+        b = (b[0]-dx, b[1]-dy)
+        ret.add(a)
+        ret.add(b)
+    return ret
+
+def antinodes(a,b):
+    return resonant_nodes(a,b, rcount=1)
 
 def take_two(nodes):
     for i in range(len(nodes)-1):
@@ -29,9 +38,9 @@ def take_two(nodes):
             yield (nodes[i], nodes[j])
 
 
-def all_antinodes(nodes):
+def all_antinodes(nodes, **kw):
     for a,b in take_two(nodes):
-        for x in antinodes(a,b):
+        for x in resonant_nodes(a,b, **kw):
             yield x
 
 def on_board_fcn(lines):
@@ -39,20 +48,47 @@ def on_board_fcn(lines):
     Ncols = len(lines[0])
     return lambda row,col: row>=0 and row<Nrows and col>=0 and col<Ncols
 
-def solA(lines):
+def all_all_antinodes(lines, rcount, initial=()):
+    """
+    all_antinodes for every line in lines also inluding
+    coordinates given in  initia.
+    """
+
     ob = on_board_fcn(lines)
 
-    antinodes = set()
+    antinodes = set((x for x in initial))
     for nodes in frequency_table(lines).values():
-        for n in all_antinodes(nodes):
+        for n in all_antinodes(nodes, rcount=rcount):
             if ob(*n):
                 antinodes.add(n)
-    return len(antinodes)
+    return antinodes
+
+def solA(lines):
+    return len(all_all_antinodes(lines, rcount=1))
 
 def solB(lines):
-    pass
+    rcount = max(len(lines), len(lines[0]))
+
+    initial = []
+    for antenna_pos in frequency_table(lines).values():
+        initial+=antenna_pos
+    
+    antennas = sum(len(x) for x in frequency_table(lines).values())
+    return len(all_all_antinodes(lines, rcount = rcount, initial=initial))
+
 
 class Test_(TestCase):
+    def test_solB(self):
+        self.assertEqual(
+            34,
+            solB(puzzle_input('example')))
+
+    def test_resonant_nodes(self):
+        self.assertEqual(
+            set(((2,4), (3,6),
+                 (-1,-2), (-2,-4))),
+            resonant_nodes((0,0), (1,2), 2))
+
     def test_solA(self):
         self.assertEqual(
             14,
@@ -64,14 +100,15 @@ class Test_(TestCase):
         self.assertEqual(
             4,
             sum(1 for anode in \
-                all_antinodes(frequency_table(puzzle_input('example_a'))['a']) \
+                all_antinodes(frequency_table(puzzle_input('example_a'))['a'],
+                              rcount=1) \
                 if sut(*anode)))
         
     def test_all_antinodes(self):
         nodes = frequency_table(puzzle_input('example_a'))['a']
         self.assertEqual(
             6,
-            len(set(all_antinodes(nodes))))
+            len(set(all_antinodes(nodes, rcount=1))))
     def test_take_two(self):
         self.assertEqual(
             set((('a','b'), ('a','c'), ('b','c'))),
@@ -86,5 +123,3 @@ class Test_(TestCase):
 print(f"A: {solA(puzzle_input('input'))}")
 print(f"B: {solB(puzzle_input('input'))}")
 #main()
-
-
