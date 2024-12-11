b3723883ecf69d7bfff5073b20a8f1e793315ac6
Fredrik Bülow <fredrik.bulow@gmail.com>
2024-12-11 06:39:29 +0100

    11a

--- 2024/11/sol.py
+++ 2024/11/sol.py
@@ -8,36 +8,46 @@ def puzzle_input(filename="input"):
     with open(filename,'r') as f:
         return [x.strip() for x in f.readlines()]
 
-all_directions= tuple((int(z.real), int(z.imag)) for\
-                      z in (1j,
-                            1j*1j,
-                            1j*1j*1j,
-                            1j*1j*1j*1j))
-    
-def trail(lines, row, col, should_be=0):
-    if (row<0) or (col<0):
-        return []
-    try:
-        c = lines[row][col]
-    except IndexError:
-        return []
-    if c == str(should_be):
-        if c == '9':
-            return [(row, col)]
-        else:
-            return sum(
-                (trail(lines, row+dr, col+dc, should_be+1)
-                 for dr, dc in all_directions), start = [])
+def trans(stone):
+    if stone == '0':
+        return ['1']
+    elif len(stone)%2==0: #Even
+        N=len(stone)//2
+        return [str(int(stone[:N])), str(int(stone[N:]))]
     else:
-        return []
+        return [str(int(stone)*2024)]
     
-def solA(lines):
-    pass
+def blink(stones):
+    return " ".join(sum((trans(stone) for stone in stones.split()), start = []))
+
+    
+def solA(lines, N=25):
+    line = lines[0]
+    for _ in range(N):
+        line=blink(line)
+    return len(line.split())
+    
+
 def solB(lines):
     pass
 
 
 class Test_(TestCase):
     def test_solA(self):
-        self.assertEqual(1,1)
+        self.assertEqual(
+            22,
+            solA(["125 17"], 6))
+        self.assertEqual(
+            55312,
+            solA(["125 17"], 25))
+
+    def test_blink(self):
+        self.assertEqual(
+            "1 2024 1 0 9 9 2021976",
+            blink("0 1 10 99 999"))
+    def test_trans(self):
+        self.assertEqual(['1'], trans('0'))
+        self.assertEqual(['2024'], trans('1'))
+        self.assertEqual(['20','24'], trans('2024'))
+            
 
